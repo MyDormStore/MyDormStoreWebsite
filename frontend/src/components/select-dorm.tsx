d8c525a96e0e6529dff7bf22870a24240678361e
WhosMadeer <mahirkhandaker@gmail.com>
2025-07-15 18:59:56 -0400

    force bottom

--- frontend/src/components/select-dorm.tsx
+++ frontend/src/components/select-dorm.tsx
@@ -145,7 +145,7 @@ export function SearchSelect({
                     <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
             </PopoverTrigger>
-            <PopoverContent className="w-full p-0" align="start">
+            <PopoverContent className="w-full p-0" align="start" side="bottom">
                 <Command>
                     <CommandInput
                         placeholder="Search..."
